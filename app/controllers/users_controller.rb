74865415846de02a52d8e72d3c15980e23f1add6
shantelraya <parischax@gmail.com>
2022-11-28 10:39:27 +0300

    done assignments

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,12 @@
 class UsersController < ApplicationController
+    def show 
+        user = User.find(params[:id])
+        render json: user, include: :items
+    end
+
+    private
+    
+    def render_not_found_response
+        render json: { error: "User not found" }, status: :render_not_found
+    end
 end
